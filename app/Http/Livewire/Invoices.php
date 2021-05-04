6200346e88878321f19380411c72f874a67d4bb9
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-05-04 13:30:42 -0500

    23 - Mostrar facturas

---
+++ app/Http/Livewire/Invoices.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Invoices extends Component
+{
+    protected $listeners =  ['render'];
+
+    public function render()
+    {
+        $invoices = auth()->user()->invoices();
+        return view('livewire.invoices',compact('invoices'));
+    }
+}
